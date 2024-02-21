cbd44a1923909dd504426e49917f3de07372c88b
Ryan Haley <me@ryanchaley.com>
2024-02-21 16:56:00 -0500

    Update main to start a bubbletea program
    This commit removes the anemic game loop that was acting as a
    placeholder in favor of starting up an actual UI loop. I went with the
    bubbletea library since charmbracelet has a very good ecosystem for TUI
    apps and should save us a lot of headaches.

--- cmd/nebula/main.go
+++ cmd/nebula/main.go
@@ -5,8 +5,31 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/haleylab/nebula/internal/app/game"
+	tea "github.com/charmbracelet/bubbletea"
 )
+
+// rch: It's possible this can just be our game.Game type, but just to get the
+//      ball rolling, I'm just going ground-up for now.
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return "hello world"
+}
 
 func main() {
 	ctx := context.Background()
@@ -17,10 +40,16 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	g := game.NewGame()
-	for {
-		if err := g.Tick(ctx); err != nil {
-			return err
-		}
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		return err
 	}
+	defer f.Close()
+
+	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		return err
+	}
+
+	return nil
 }
